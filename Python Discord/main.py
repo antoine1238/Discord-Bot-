55da5ab28faca47124b77a99fd4b05634adc20d3
antoine manuel <antoinemg1238@gmail.com>
2021-03-24 18:11:32 +0100

    aqui tenemos lo basico

---
+++ Python Discord/main.py
@@ -0,0 +1,56 @@
+import discord
+from discord.ext import commands
+import datetime
+
+from urllib import parse, request
+import re
+
+# Con esto asignamos el caracter especial para darle instrucciones al bot. description es opcional
+bot = commands.Bot(command_prefix=">", description="I am the discord robot :)")
+
+# ------FUNCIONES------
+# las funciones tienen que ser async/await
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
+
+@bot.command()
+async def suma(ctx, num1: int, num2: int):
+    await ctx.send(num1 + num2)
+
+@bot.command()
+async def info(ctx):
+    """Embed reprecenta un cuadro en discord en donde podemos poner 
+        titulos, contenido imagenes fecha etc."""
+   
+    embed = discord.Embed(title=f"{ctx.guild.name}",    # ctx.guild.name = nombre del servidor
+        description="lorem ipsum lo que sea bla bla",
+        timestamp=datetime.datetime.utcnow(),           # tiempo 
+        color=discord.Color.blue()                      # color de la caja 
+        )
+    embed.add_field(name="Server Created at", value = f"{ctx.guild.created_at}") # cuando fue creado el server
+    embed.add_field(name="Server Owner", value = f"{ctx.guild.owner}")           # quien es el dueño
+    embed.add_field(name="Server Region", value = f"{ctx.guild.region}")         # cual es la region
+    embed.add_field(name="Server id", value = f"{ctx.guild.id}")                 # cual es el id del server
+    embed.set_thumbnail(url = "https://images6.alphacoders.com/523/523425.jpg")  # para mandar una imagen. {ctx.guild.icon} = icono del server
+        
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def youtube(ctx, *, search):
+    query_search = parse.urlencode({"search_query": search}) # transforma palabras comunes en direcciones a internet. ejemplo: quita espacios y agrega % etc.
+    html_content = request.urlopen("https://www.youtube.com/results?" + query_search)
+    search_results = re.findall( r"watch\?v=(\S{11})", html_content.read().decode()) # para obtener los id de los videos
+    await ctx.send("https://www.youtube.com/watch?v=" + search_results[0]) # envia una caja con el primer video conseguido :)
+
+# Events
+@bot.event
+async def on_ready():
+    # podemos cambiar el estado del robot. en este caso a transmitiendo y incluso darle un link para la url 
+    await bot.change_presence(activity=discord.Streaming(name="Python-discord", url="https://www.youtube.com/watch?v=05r7WsIXT-A&ab_channel=FaztCode"))
+
+    print("The Robot is alive") # cuando se conecte dirá esto
+
+
+
+bot.run("ODI0MzA1OTQ4MDYzMDM5NTc4.YFtcvA.SsPUx27l0v1emRq09ajUvRNdRJI")
---
+++ Readme.txt
@@ -0,0 +1,9 @@
+- ingresar a discord dev para poder crear una aplicacion y luego un robot
+
+- en "robot" hay un token el cual deberas copiar, este es como la contraseña de tu bot.. 
+    tienes que poner ese token en tu código ' bot.run("<aqui va el token>") '
+
+- en "OAuth2" hay una caja la cual contiene una opcion llamada "robot", seleccionamos y debajo apareceran los permisos que tendra el bot
+
+- entre las cajas habrá un código de url el cual copiaremos y abriremos en una pestaña nueva, esto es para finalmente asignar el robot a uno de tus servidores
+
